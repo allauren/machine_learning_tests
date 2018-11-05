31b64614ffb8452a12283bfcd7c513b9d1c44aaa
allauren <allauren@MacBook-Pro-de-MMI.local>
2018-11-05 04:47:40 +0100

    adding all the tests made

---
+++ ex03/tagger.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
+ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+
+new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
+
+words = word_tokenize(new_text)
+words = [w for w in words if not w in stop_words]
+
+filtered_sentence = [ps.stem(w) for w in words]
+print(words)
+print(filtered_sentence)
