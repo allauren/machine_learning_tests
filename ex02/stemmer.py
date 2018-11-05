569870f94411c7d552867d223f77a2d9763a282e
allauren <allauren@macbook-pro-de-mmi.local>
2018-11-05 08:24:56 +0100

    test are going ok

--- ex02/stemmer.py
+++ ex02/stemmer.py
@@ -8,7 +8,7 @@ stop_words = set(stopwords.words('english'))
 new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
 
 words = word_tokenize(new_text)
-words = [w for w in words if not w in stop_words]
+words = [w for w in words if w not in stop_words]
 
 filtered_sentence = [ps.stem(w) for w in words]
 print(words)
--- ex05/chinker.py
+++ ex05/chinker.py
@@ -11,13 +11,15 @@ tokenized = custom_sent_tokenizer.tokenize(sample_text)
 
 def process_content():
     try:
-        for i in tokenized:
+        for i in tokenized[5:]:
             words = nltk.word_tokenize(i)
             tagged = nltk.pos_tag(words)
-            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}"""
+
+            chunkGram = r"""Chunk: {<.*>+}
+                                    }<VB.?|IN|DT|TO>+{"""
+
             chunkParser = nltk.RegexpParser(chunkGram)
             chunked = chunkParser.parse(tagged)
-            chunked.draw()     
 
     except Exception as e:
         print(str(e))
