391da5ccac2a1ec0fa565f87789d2db0b824653c
allauren <allauren@student.42.fr>
2018-11-07 15:12:30 +0100

    first model trained

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -1,5 +1,7 @@
 import gensim
+import matplotlib.pyplot as plt
 
 model = gensim.models.Word2Vec.load("word2vec.model")
-print(model.wv.most_similar (positive='reliable', topn=30))
+print(model.wv.most_similar (positive='fun', topn=30))
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
 
